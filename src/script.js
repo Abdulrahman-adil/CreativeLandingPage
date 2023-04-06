1985cd76580f47cd23c32399d79771d7db4705b4
abdo adel <abdo48439@gmail.com>
2023-04-06 16:41:05 +0200

    Git IMg Sec

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let theIMg = document.getElementById('img');
+
+// theIMg.addEventListener('click',  (phone) => {
+
+//     theIMg.src = phone;
+//     console.log('test')
+// })
+function changePhone(phone){
+theIMg.src = phone;
+console.log('test ')
+
+}
+console.log('test javaSCript')
